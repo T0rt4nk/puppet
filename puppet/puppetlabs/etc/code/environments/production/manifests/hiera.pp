78df7c463023ee27704eddb0b72118b47b0e9e0c
Maxime Vidori <maxime.vidori@gmail.com>
2016-08-25 09:35:43 +0200

    Remove hiera demo file

--- puppet/puppetlabs/etc/code/environments/production/manifests/hiera.pp
+++
@@ -1,5 +0,0 @@
-file {'/tmp/example-ip':                                            # resource type file and filename
-	ensure  => present,                                               # make sure it exists
-	mode    => '0644',                                                # file permissions
-	content => hiera('max.password'),  # note the ipaddress_eth0 fact
-}
